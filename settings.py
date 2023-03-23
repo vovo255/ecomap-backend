a88910cab02b32c3a6ee24f600d8d945091b086f
Владимир Алексеев <it@vladimirva.ru>
2023-03-23 14:49:06 +0300

    Change db connector string

--- settings.py
+++ settings.py
@@ -1,2 +1,3 @@
-DB_CONN_STR = r"D:\ecomap-backend\db\main.sqlite"
+#DB_CONN_STR = r"D:\ecomap-backend\db\main.sqlite"
+DB_CONN_STR = r"/home/vovo256/ecomap-backend/db/main.sqlite"
 TOKEN_LIVE_TIME_S = 3600
