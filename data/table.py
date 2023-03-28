580e1254d21f97e05b7db695f7bb1ebea137c93c
Владимир Алексеев <it@vladimirva.ru>
2023-03-28 22:43:58 +0300

    user scheme changed. Rate column added

--- data/table.py
+++ data/table.py
@@ -16,6 +16,7 @@ class User(SqlAlchemyBase, SerializerMixin):
     nickname = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     gender = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
     email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
+    rate = sqlalchemy.Column(sqlalchemy.Integer, nullable=False, default=10)
     password_hash = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     salt = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     create_date = sqlalchemy.Column(sqlalchemy.Integer, nullable=False, default=int(time.time()))
